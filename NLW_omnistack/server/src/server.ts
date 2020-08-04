dee516faebe77755572b393303c1ea2ac4770f22
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-08-04 17:43:44 -0300

    Criação das tabelas da Banco de Dados

--- NLW_omnistack/server/src/server.ts
+++ NLW_omnistack/server/src/server.ts
@@ -1,24 +1,10 @@
 //console.log('Hello World!');
 import express from 'express';
+import routes from './routes'
 
 const app = express();
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World!'})
-    {
-    //console.log('Acessou a rota');
-    //return response.send('Hello World!');
-    /*
-    console.log(request.body)
-
-    const users = [
-        { name: 'Diego', age: 25 },
-        { name: 'Luís', age: 20 }
-    ];
-    return response.json(users);*/
-    }
-});
+app.use(routes);
 
 // http://localhost:33333
 app.listen(3333);
