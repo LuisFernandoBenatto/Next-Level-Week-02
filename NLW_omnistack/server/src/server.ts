c0203c01495d4586caf5203f5815e25c608c4eaf
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-08-04 18:56:00 -0300

    Segundo dia da NLW #02

--- NLW_omnistack/server/src/server.ts
+++ NLW_omnistack/server/src/server.ts
@@ -1,10 +1,12 @@
 //console.log('Hello World!');
 import express from 'express';
-import routes from './routes'
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 // http://localhost:33333
 app.listen(3333);
