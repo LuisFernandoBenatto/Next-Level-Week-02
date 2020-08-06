ad25e77f1aabd2b907ed91efb309b7dfc2e84ea7
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2020-08-06 16:23:20 -0300

    Finaliznado terceiro dia de nlw

--- NLW_omnistack/web/src/components/TeacherItem/index.tsx
+++ NLW_omnistack/web/src/components/TeacherItem/index.tsx
@@ -1,41 +1,48 @@
 import React from 'react'
+import api from '../../services/api'
 import './styles.css'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    name: string;
+    avatar: string;
+    bio: string;
+    cost: number;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return(
         <article className='teacher-item'>
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt=""/>
+                <img src={ teacher.avatar } alt={ teacher.name }/>
                 <div>
-                    <strong>Diego Fernandes</strong>
-                    <span>Química</span>
+                    <strong>{ teacher.name }</strong>
+                    <span>{ teacher.subject }</span>
                 </div>
             </header>
-            <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br/><br/>
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-            </p>
-            <p>
-            Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar.
-            <br/><br/>
-            Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: "Aprenda a fazer dinheiro com isso!"
-            </p>
-            <p>
-            As vezes não sei nem onde eu tô, mas consigo me localizar facilmente em qualquer lugar. Tenho memória fotográfica e nunca fico perdido.
-            <br/><br/>
-            Eu ensino a galera como não se perder na vida, com lições geográficas simples pra você nunca mais precisar de mapa na sua bela vida.
-            </p>
+            <p>{ teacher.bio }</p>
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
-                    <button type="button">
+                    <strong>R$ { teacher.cost }</strong>
+                    <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${ teacher.whatsapp }`}>
                         <img src={whatsappIcon} alt="Whatsapp"/>
                         Entrar em contato
-                    </button>
+                    </a>
                 </p>
             </footer>
         </article>
--- NLW_omnistack/web/src/pages/TeacherForm/index.tsx
+++ NLW_omnistack/web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Textarea from '../../components/TextArea';
 import Select from '../../components/Select';
+import { useHistory } from 'react-router-dom';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
+import api from '../../services/api';
 
 function TeacherForm() {
+    const history = useHistory();
+
     const [ name, setName ] = useState('');
     const [ avatar, setAvatar ] = useState('');
     const [ whatsapp, setWhatsapp ] = useState('');
@@ -16,13 +20,50 @@ function TeacherForm() {
     const [ subject, setSubject ] = useState('');
     const [ cost, setCost ] = useState('');
 
-    function handleCreateClass() {
-        
-    }
-
     const [scheduleItems, setScheduleItems] = useState([
         { week_day: 0,  from: '', to: '' }
     ]);
+
+    function handleCreateClass(e: FormEvent) {
+        e.preventDefault();
+
+        api.post('classes', {
+            name, 
+            avatar, 
+            whatsapp, 
+            bio, 
+            subject, 
+            cost: Number(cost),
+            schedule: scheduleItems
+        }).then(() => {
+            alert('Cadastro realizado com sucesso!');
+
+            history.push('/');
+        }).catch(() => {
+            alert('Erro no cadastro!');
+        })
+        console.log
+        ({
+            name, 
+            avatar, 
+            whatsapp, 
+            bio, 
+            subject, 
+            cost,
+            scheduleItems
+        });
+    }
+
+    function setScheduleItemValue(position: number, field: string, value: string) {
+        const updatedScheduleItems = scheduleItems.map((scheduleItem, index) => {
+            if(index === position) {
+               return { ...scheduleItem, [field]: value };
+            }
+            return scheduleItem;
+        });
+        setScheduleItems(updatedScheduleItems);
+    }
+    
     function addNewScheduleItem() {
         setScheduleItems([
             ...scheduleItems,
@@ -36,7 +77,8 @@ function TeacherForm() {
             title = "Que incrível que você quer dar aulas." 
             description = "O primeiro passo é preencher esse formulário de inscrição"
         />
-            <main>
+            <main onSubmit={handleCreateClass}>
+            <form>
                 <fieldset>
                     <legend>Seus Dados</legend>
                     <Input 
@@ -64,7 +106,6 @@ function TeacherForm() {
                         onChange={(e) => {setBio(e.target.value)}}
                     />
                 </fieldset>
-
                 <fieldset>
                     <legend>Sobre a Aula</legend>
                     <Select 
@@ -96,7 +137,6 @@ function TeacherForm() {
                         onChange={(e) => {setCost(e.target.value)}} 
                     />
                 </fieldset>
-
                 <fieldset>
                     <legend>
                         Horários disponíveis
@@ -104,12 +144,14 @@ function TeacherForm() {
                             + Novo horário
                         </button>
                     </legend>
-                    {scheduleItems.map(scheduleItem => {
+                    {scheduleItems.map((scheduleItem, index) => {
                         return (
                             <div key={scheduleItem.week_day} className="schedule-item">
                                 <Select 
                                     name="week_day"
-                                    label="Dia da semana" 
+                                    label="Dia da semana"
+                                    value={scheduleItem.week_day} 
+                                    onChange={(e) => {setScheduleItemValue(index, 'week_day', e.target.value)}}
                                     options ={[
                                         { value: '0', label: 'Domingo' },
                                         { value: '1', label: 'Segunda-feira' },
@@ -120,24 +162,36 @@ function TeacherForm() {
                                         { value: '6', label: 'Sábado' },
                                     ]}
                                 />
-                                <Input name="from" label="Das" type="time"/>
-                                <Input name="to" label="Até" type="time"/>
+                                <Input 
+                                    name="from" 
+                                    label="Das" 
+                                    type="time"
+                                    value={scheduleItem.from}
+                                    onChange={(e) => {setScheduleItemValue(index, 'from', e.target.value)}}
+                                />
+                                <Input 
+                                    name="to" 
+                                    label="Até" 
+                                    type="time"
+                                    value={scheduleItem.to}
+                                    onChange={(e) => {setScheduleItemValue(index, 'to', e.target.value)}}
+                                />
                             </div> 
                         );
                     })}
                      
                 </fieldset>
-
                 <footer>
                     <p>
                         <img src={ warningIcon } alt="Aviso inportante"/>
                         Importante! <br />
                         Preencha todos os dados
                     </p>
-                    <button type="button">
+                    <button type="submit">
                         Salvar cadastro
                     </button>
                 </footer>
+                </form>
             </main>
         </div>
     );
